fc2e9c34bbf7a96262a92bf8339ce6ff01ea2a10
NTKKS <janasja1@gmail.com>
2018-10-20 10:26:36 +0200

    ukol ze cvika

--- src/cz/uhk/fim/rssreader/gui/MainFrame.java
+++ src/cz/uhk/fim/rssreader/gui/MainFrame.java
@@ -4,14 +4,19 @@ import cz.uhk.fim.rssreader.utils.FileUtils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 
 public class MainFrame extends JFrame {
-    public MainFrame(){init();}
 
-        private void init(){
+    public MainFrame() {
+        init();
+    }
+
+    private void init() {
         setTitle("RSS Reader");
-        setSize(800,600);
+        setSize(800, 600);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
@@ -22,29 +27,65 @@ public class MainFrame extends JFrame {
         //horni panel
         JPanel controlPanel = new JPanel(new BorderLayout());
 
+        //horní itemy
         JButton btnLoad = new JButton("Load");
         JTextField txtInputField = new JTextField();
         JButton btnSave = new JButton("Save");
+        JLabel lblError = new JLabel("Prazdne pole!!!",JLabel.CENTER);
+        lblError.setBackground(Color.RED);
+        lblError.setOpaque(true);
+        //Pridani hornich itemů
+        controlPanel.add(btnLoad, BorderLayout.WEST);
+        controlPanel.add(txtInputField, BorderLayout.CENTER);
+        controlPanel.add(btnSave, BorderLayout.EAST);
 
-        controlPanel.add(btnLoad,BorderLayout.WEST);
-        controlPanel.add(txtInputField,BorderLayout.CENTER);
-        controlPanel.add(btnSave,BorderLayout.EAST);
+        //listener tlacitka Load
+        btnLoad.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("Load clicked");
+                if (validateInput(txtInputField.getText())==false){
+                    controlPanel.add(lblError, BorderLayout.NORTH);
+                    controlPanel.updateUI();
+                }else {controlPanel.remove(lblError);
+                controlPanel.updateUI();}
 
-        add(controlPanel,BorderLayout.NORTH);
+            }
+        });
+        //listener tlacitka Save
+        btnSave.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("Save clicked");
+            }
+        });
+
+        //horni kontrolní panel
+        add(controlPanel, BorderLayout.NORTH);
 
         //dolni text panel
         JPanel textPanel = new JPanel(new BorderLayout());
 
         JTextArea txtContent = new JTextArea();
 
-        textPanel.add(txtContent,BorderLayout.CENTER);
+        textPanel.add(txtContent, BorderLayout.CENTER);
 
-        add(new JScrollPane(textPanel),BorderLayout.CENTER);
+        add(new JScrollPane(textPanel), BorderLayout.CENTER);
 
         try {
             txtContent.setText(FileUtils.loadStringFromFile("rss.xml"));
         } catch (IOException e) {
             System.out.println(e.getMessage());
+        }
+    }
+
+    private boolean validateInput(String in) {
+        System.out.println("validating " + in);
+        if (in.trim().equals("")) {
+            System.out.println("empty");
+            return false;
+        } else {
+            return true;
         }
     }
 }
