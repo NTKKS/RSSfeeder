8f377a93ed56aed04197930b1d758b74c0b2ddcd
janasja1 <janasja1@uhk.cz>
2018-11-05 14:36:50 +0100

    html is a thing

--- src/cz/uhk/fim/rssreader/gui/CardView.java
+++ src/cz/uhk/fim/rssreader/gui/CardView.java
@@ -13,20 +13,23 @@ public class CardView extends JPanel {
 
     final String startHtml = "<html><p style='width: " + COMPONENT_WIDTH +" px'>";
     final String endHtml = "</p></html>";
+    private Color textColor;
 
     public CardView(RSSItem item){
         setLayout(new WrapLayout());
         setSize(ITEM_WIDTH,HEIGHT);
         setBackground(genColor(item));
+        setInverseColor(genColor(item));
         setTitle(item.getTitle());
         setDescription(item.getDescription());
-        setInfo(String.format("%s - %s", item.getPubDate(), item.getAuthor()));
+        setInfo(String.format("%s%s", item.getPubDate(), item.getAuthor()));
     }
 
     private void  setTitle(String title){
         JLabel lblTitle = new JLabel();
         lblTitle.setSize(COMPONENT_WIDTH,HEIGHT);
         lblTitle.setFont(new Font("Courier", Font.BOLD, 12));
+        lblTitle.setForeground(textColor);
         lblTitle.setText(String.format("%s%s%s", startHtml, title.subSequence(0,Math.min(50,title.length()))+"...", endHtml));
         add(lblTitle);
     }
@@ -35,7 +38,8 @@ public class CardView extends JPanel {
         JLabel lblDescr = new JLabel();
         lblDescr.setSize(COMPONENT_WIDTH,HEIGHT);
         lblDescr.setFont(new Font("Courier", Font.PLAIN, 11));
-        lblDescr.setText(String.format("%s%s%s", startHtml, description.subSequence(0,Math.min(250,description.length()))+" ...", endHtml));
+        lblDescr.setForeground(textColor);
+        lblDescr.setText(String.format("%s%s%s", startHtml, description.subSequence(0,Math.min(100,description.length()))+" ...", endHtml));
         add(lblDescr);
     }
 
@@ -44,7 +48,7 @@ public class CardView extends JPanel {
         lblInfo.setSize(COMPONENT_WIDTH,HEIGHT);
         lblInfo.setFont(new Font("Courier", Font.ITALIC, 10));
         lblInfo.setForeground(Color.LIGHT_GRAY);
-        lblInfo.setText(String.format("%s%s%s", startHtml, info, endHtml));
+        lblInfo.setText(String.format("%s%s<br />%s%s", startHtml, info.substring(0,29),info.substring(29), endHtml));
         add(lblInfo);
     }
 
@@ -52,5 +56,9 @@ public class CardView extends JPanel {
         String text = item.getDescription();
         Color color = Color.decode(String.valueOf(text.hashCode()));
         return color;
+    }
+
+    private void setInverseColor(Color bgColor){
+        textColor = new Color(255-bgColor.getRed(),255-bgColor.getGreen(),255-bgColor.getBlue());
     }
 }
